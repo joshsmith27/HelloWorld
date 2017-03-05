5074f37bde15c9a7721fbe1114a623aebe191e20
Joshua Richard Smith <veryjoshua@gmail.com>
2017-03-04 22:03:58 -0700

    finished project

--- public/JS/controllers/intro.js
+++ public/JS/controllers/intro.js
@@ -1,6 +1,7 @@
-var controller = new ScrollMagic.Controller();
-var scene = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 300})
-        // animate color and top border in relation to scroll position
-        .setTween("#animate2", {borderTop: "30px solid white", backgroundColor: "blue", scale: 0.7}) // the tween durtion can be omitted and defaults to 1
-        .addIndicators({name: "2 (duration: 300)"}) // add indicators (requires plugin)
-        .addTo(controller);
+
+$( document ).ready(function() {
+  $('.main-carousel').flickity({
+    // options
+  wrapAround: true
+  });
+});
--- public/JS/controllers/introAngularCtrl.js
+++ public/JS/controllers/introAngularCtrl.js
@@ -1,31 +1,3 @@
 app.controller('introAngularCtrl', function($scope, $timeout){
-$scope.shrink ={};
-$scope.showPortfolioPicture ={};
-$scope.portfolioPictureDexriptionContainer ={
-  'display' : 'none',
-};
-  $timeout(function () {
-    $scope.shrink = {
-      'opacity' : '0',
-    };
-  }, 5000);
-  $timeout(function () {
-    $scope.portfolioPictureDexriptionContainer ={
-      'display' : 'block'
-    };
-  }, 9000);
-
-  $scope.shrinkContainer = {};
-  $timeout(function () {
-
-    $scope.shrinkContainer ={
-      "margin-top" : "0px"
-    };
-    $scope.showPortfolio = {
-      'opacity' : '1'
-    };
-    $scope.shrink ={
-      "display" : "none"
-    };
-  }, 9500);
+  
 });
