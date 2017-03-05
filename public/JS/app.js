0f64ccc7590d8fcf7ad1da9bd99d7f9b070a7eb8
Joshua Richard Smith <veryjoshua@gmail.com>
2017-03-04 22:58:39 -0700

    code clean

--- public/JS/app.js
+++ public/JS/app.js
@@ -5,14 +5,6 @@ var app = angular.module('joshuarsmith', ['ui.router'])
     url:'/Intro',
     templateUrl:'/views/intro.html',
     controller: 'introAngularCtrl',
-  })
-  .state('resume',{
-    url:'/Resume',
-    templateUrl:'/views/resume.html',
-  })
-  .state('coverletter',{
-    url:'/CoverLetter',
-    templateUrl:'/views/coverletter.html',
   });
   $urlRouterProvider
   .otherwise('/Intro');
